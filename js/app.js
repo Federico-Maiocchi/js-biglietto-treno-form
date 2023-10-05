93698c6a9506ebf2a532e24bbadcb6d6fd1db069
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-10-05 16:33:21 +0200

    ADD: section if discount

--- js/app.js
+++ js/app.js
@@ -14,13 +14,19 @@ let priceBaseKilometre;
 priceBaseKilometre = 0.21;
 console.log ('prezzo base kilometro', priceBaseKilometre);
 
+// Discount
+const selDomElement = document.getElementById('discount');
+
+// section year
+let year = parseInt(selDomElement.value)
+
 // Button calc
 const btnCalcDOMElement = document.getElementById('btn-calc');
 console.log(btnCalcDOMElement);
 
 btnCalcDOMElement.addEventListener('click', function () {
 
-	console.log(btnCalcDOMElement.value + 'Ho cliccato');
+	console.log(selDomElement.value + 'Ho cliccato');
 
     const kmPassenger = parseFloat(kmPassengerDomElement.value)
     console.log('Km passenger ' + kmPassenger)
@@ -28,10 +34,32 @@ btnCalcDOMElement.addEventListener('click', function () {
     const totalPrice = kmPassenger * priceBaseKilometre;
     console.log('Prezzo totale ' + totalPrice);
 
-    
+    if ( year == "minorenne") {
+        //         - applicare lo sconto del 20%
+        selDomElement = totalPrice * (20 / 100);
+        console.log('sconto minori',discount);
+        }
+        //     - ALTRIMENTI SE è > 65 anni
+        else if (year == "over65") {
+        //         - applicare lo sconto del 40% 
+        selDomElement = totalPrice * (40 / 100);
+        console.log('sconto over 65',discount);
+        }
+        // - ALTRIMENTI
+        else (year == "maggiorenne") {
+        selDomElement = 0;
+        //          - non si hanno sconti
+        console.log('niente sconto');
+        }
 
-	
-	
+        let totalPriceDiscount = totalPrice - discount;
+        console.log ('prezzo totale sconto');
+        // console.log(totalPriceDiscount)
+        
+        totalPriceDiscount = totalPriceDiscount.toFixed(2);
+        console.log(totalPriceDiscount);
+
+
 })
 // - tasto annulla, resettare le impostazioni
 
